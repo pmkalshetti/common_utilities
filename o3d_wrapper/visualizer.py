dd4f215bb3d38a618dd658483058a21b5a28e8db
pmkalshetti <pmkalshetti@users.noreply.github.com>
2020-01-16 16:36:43 +0530

    update visualizer for open3d 0.8

--- o3d_wrapper/visualizer.py
+++ o3d_wrapper/visualizer.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import open3d as o3d
-import tensorflow as tf
 
 
 class Visualizer:
@@ -10,23 +9,13 @@ class Visualizer:
     Attributes
     ----------
     vis : o3d.Visualizer
-
-    width, height : int
-        Dimensions of window.
-
-    fx, fy : float
-        Focal lengths in pixel.
-    cx, cy : float
-        Camera principal point.
-    pos_cam : list of float
-        3D position of camera.
-
-    view_control : o3d.Visualizer.View_control
     """
 
-    def __init__(self, window_name="Visualizer", width=640, height=480,
-                 left=50, top=50, path_render_option=None, fx=475, fy=475,
-                 pos_cam=[0, 0, 0]):
+    def __init__(self, window_name="Visualizer", left=50, top=50,
+                 width=640, height=480, fx=475, fy=475, pos_cam=[0, 0, 0],
+                 background_color=[1.0, 1.0, 1.0],
+                 point_size=5.0,
+                 mesh_show_wireframe=True, mesh_shade_option=0):
         """Creates a visualizer with given properties.
 
         Arguments
@@ -34,14 +23,11 @@ class Visualizer:
         window_name : string
             Name on title bar.
 
-        width, height : int
-            Dimensions of window.
-
         left, top : int
             Top-left corner position of window.
 
-        path_render_option : string
-            Path to json file containing render options for visualizer.
+        width, height : int
+            Dimensions of window.
 
         fx, fy : float
             Focal lengths in pixel.
@@ -49,46 +35,59 @@ class Visualizer:
         pos_cam : list of float
             3D position of camera.
         """
-        self.width = width
-        self.height = height
-        self.fx = fx
-        self.fy = fy
-        self.cx = self.width/2 - 0.5
-        self.cy = self.height/2 - 0.5
-        self.pos_cam = pos_cam
-
         self.vis = o3d.visualization.VisualizerWithKeyCallback()
+
+        self.create_window(window_name, width, height, left, top)
+        self.set_render_option(
+            background_color, point_size,
+            mesh_show_wireframe, mesh_shade_option
+        )
+        self.set_view(
+            width, height,
+            fx, fy,
+            width/2 - 0.5, height/2 - 0.5,
+            pos_cam
+        )
+
+    def create_window(self, window_name, width, height, left, top):
         cwd = os.getcwd()  # to handle issue on Mac
-        self.vis.create_window(window_name=window_name,
-                               width=width,
-                               height=height,
-                               left=left,
-                               top=top)
+        self.vis.create_window(
+            window_name=window_name,
+            width=width, height=height, left=left, top=top
+        )
         os.chdir(cwd)  # to handle issue on Mac
 
-        if path_render_option is not None:
-            render_option = self.vis.get_render_option()
-            render_option.load_from_json(path_render_option)
+    def set_render_option(self, background_color,
+                          point_size,
+                          mesh_show_wireframe, mesh_shade_option):
+        render_option = self.vis.get_render_option()
+        render_option.background_color = background_color
+        render_option.point_size = point_size
+        render_option.mesh_show_wireframe = mesh_show_wireframe
+        # render_option.mesh_shade_option = mesh_shade_option
+        # render_option.load_from_json(path_render_option)
 
-        self.view_control = self.vis.get_view_control()
-        self.set_view()
-
-    def set_view(self):
+    def set_view(self, width, height, fx, fy, cx, cy, pos_cam):
         """Sets camera view."""
-        self.view_control.scale(1)
-        camera_intrinsic = o3d.PinholeCameraIntrinsic(
-            self.width, self.height, self.fx, self.fy, self.cx, self.cy)
+        view_control = self.vis.get_view_control()
+        view_control.set_constant_z_far(3000)
+        view_control.scale(1)
+
+        camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
+            width, height, fx, fy, cx, cy
+        )
         camera_extrinsic = np.array([
-            [1, 0, 0, self.pos_cam[0]],
-            [0, 1, 0, self.pos_cam[1]],
-            [0, 0, 1, self.pos_cam[2]],
+            [1, 0, 0, pos_cam[0]],
+            [0, 1, 0, pos_cam[1]],
+            [0, 0, 1, pos_cam[2]],
             [0, 0, 0, 1]
         ], dtype=np.float32)
-        pinhole_camera_parameters = o3d.PinholeCameraParameters()
+        pinhole_camera_parameters = o3d.camera.PinholeCameraParameters()
         pinhole_camera_parameters.intrinsic = camera_intrinsic
         pinhole_camera_parameters.extrinsic = camera_extrinsic
-        self.view_control.convert_from_pinhole_camera_parameters(
-            pinhole_camera_parameters)
+        view_control.convert_from_pinhole_camera_parameters(
+            pinhole_camera_parameters
+        )
 
     def add_mesh(self, mesh):
         """Add a mesh to the visualizer.
@@ -98,7 +97,6 @@ class Visualizer:
         mesh : `lib.o3d_wrapper.Mesh` object
         """
         self.vis.add_geometry(mesh.mesh)
-        self.set_view()
 
     def add_pcd(self, pc):
         """Add a pcd to the visualizer.
@@ -108,7 +106,6 @@ class Visualizer:
         pc : `lib.o3d_wrapper.PointCloud` object
         """
         self.vis.add_geometry(pc.pcd)
-        self.set_view()
 
     def add_lineset(self, lineset):
         """Add lineset to visualizer.
@@ -118,14 +115,19 @@ class Visualizer:
         lineset : `lib.o3d_wrapper.Lineset` object.
         """
         self.vis.add_geometry(lineset.lineset)
-        self.set_view()
 
     def show_frame(self, pos=np.array([0, 0, 0]), scale=100):
         """Adds a coordinate frame in visualizer."""
-        frame = o3d.geometry.create_mesh_coordinate_frame(
-            size=scale, origin=pos)
+        frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=scale, origin=pos
+        )
         self.vis.add_geometry(frame)
-        self.set_view()
+
+    def update(self, geometries):
+        if not isinstance(geometries, list):
+            geometries = [geometries]
+
+        [self.vis.update_geometry(geometry) for geometry in geometries]
 
     def show(self):
         """Updates geometries in visualizer.
@@ -135,7 +137,7 @@ class Visualizer:
         open_window : bool
             `False` if window is to be closed.
         """
-        self.vis.update_geometry()
+        # self.vis.update_geometry()
         open_window = self.vis.poll_events()
 
         return open_window
@@ -177,7 +179,15 @@ class Visualizer:
         return img
 
     def get_cam(self):
-        return tf.constant((self.fx, self.fy, self.cx, self.cy))
+        pinhole_camera_parameters = \
+            self.vis.convert_to_pinhole_camera_parameters()
+        camera_intrinsic = pinhole_camera_parameters.intrinsic
+        camera_extrinsic = pinhole_camera_parameters.extrinsic
+
+        fx, fy = camera_intrinsic.get_focal_length()
+        cx, cy = camera_intrinsic.get_principal_point()
+
+        return np.stack((fx, fy, cx, cy))
 
     def __del__(self):
         self.vis.destroy_window()
@@ -186,3 +196,11 @@ class Visualizer:
     def save_img(self, path):
         """Saves visualizer window as uint8 image."""
         self.vis.capture_screen_image(path, True)
+
+
+if __name__ == "__main__":
+    visualizer = Visualizer()
+    visualizer.show_frame()
+    while True:
+        if not visualizer.show():
+            break
